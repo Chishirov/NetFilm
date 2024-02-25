c7946907106b91d0a6bd7a5daba331cd88c74a7e
omar <yossef>
2024-02-25 21:46:58 +0100

    new update

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,21 +1,15 @@
 import { Router } from "express";
 import {
-  postLoginController,
-  postLogoutController,
+  postLoginUser,
+  postSignoutUser,
   postRegisterUser,
   getValidateUser,
 } from "../controller/userController.js";
-import { postMovieId } from "../controller/moviesController.js";
 
 export const router = Router();
-// Middleware to set ETag header for routes involving tokens and cookies
-function setETag(req, res, next) {
-  res.set("etag", "strong");
-  next();
-}
+
 router
   .post("/register", postRegisterUser)
-  .post("/login", setETag, postLoginController) // Apply the middleware to the login route
-  .get("/validate", setETag, getValidateUser) // Apply the middleware to the validate route
-  .post("/signout", setETag, postLogoutController) // Apply the middleware to the signout route
-  .post("/movie/:id", postMovieId);
+  .post("/login", postLoginUser)
+  .get("/validate", getValidateUser)
+  .post("/signout", postSignoutUser);
