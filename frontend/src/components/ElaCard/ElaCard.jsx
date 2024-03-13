6552f9234e47286c6e998febc6b4b1f005d81592
EliSH-Git <elaheh.shabanian@gmail.com>
2024-03-13 13:58:52 +0100

    Add

--- frontend/src/components/ElaCard/ElaCard.jsx
+++ frontend/src/components/ElaCard/ElaCard.jsx
@@ -2,18 +2,18 @@ import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import dayjs from "dayjs";
 import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
-  Button,
-  Tooltip,
-  IconButton,
-  Menu,
-  MenuHandler,
-  MenuList,
-  MenuItem,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+    Tooltip,
+    IconButton,
+    Menu,
+    MenuHandler,
+    MenuList,
+    MenuItem,
 } from "@material-tailwind/react";
 import PosterFallback from "../../assets/no-poster.png";
 import CircleRating from "../circleRating/CircleRating";
@@ -23,7 +23,6 @@ import axios from "axios";
 import { UserContext } from "../../context/UserContext";
 
 const ElaCard = ({ data, mediaType }) => {
-
     const [openMenu, setOpenMenu] = useState(false);
     const { user, setUser } = useContext(UserContext);
     const navigate = useNavigate();
@@ -75,95 +74,51 @@ const ElaCard = ({ data, mediaType }) => {
             console.error(error);
 
 
-  // console.log(data);
+        }
+    };
+    const watchlistHandler = async (movieTitle, cardId, userId, imageUrl) => {
+        try {
+            const movieRes = await axios.post(
+                " http://localhost:3000/favorite-movie",
+                {
+                    title: movieTitle,
+                    movieId: cardId.toString(),
+                    imageUrl: imageUrl,
+                    userId: userId,
+                    isFavorite: false,
+                    isWatchlist: true,
+                },
+                { withCredentials: true }
+            );
+            if (movieRes.status === 201) {
+                // Check if the movie is already in the user's list
+                if (!user.movies.some((movie) => movie?.movieId === cardId)) {
+                    // Add the movie to the user's list
+                    setUser((prevUser) => ({
+                        ...prevUser,
+                        movies: [
+                            ...prevUser.movies,
+                            {
+                                title: movieTitle,
+                                movieId: cardId,
+                                imageUrl: imageUrl,
+                                isFavorite: true,
+                                isWatchlist: false,
+                            },
+                        ],
+                    }));
+                    setOpenMenu(!openMenu);
+                }
+            } else {
+                // Movie already exists in user's list
+                console.log("Movie already exists in user's list");
+            }
+        } catch (error) {
+            console.error(error);
+        }
 
-  const handleClick = () => {
-    console.log("clicked");
-    setOpenMenu(!openMenu);
-  };
-  const favoriteHanlder = async (movieTitle, cardId, userId, imageUrl) => {
-    try {
-      const movieRes = await axios.post(
-        " http://localhost:3000/favorite-movie",
-        {
-          title: movieTitle,
-          movieId: cardId.toString(),
-          userId: userId,
-          imageUrl: imageUrl,
-          isFavorite: true,
-          isWatchlist: false,
-        },
-        { withCredentials: true }
-      );
-      if (movieRes.status === 201) {
-        // Check if the movie is already in the user's list
-        if (!user.movies.some((movie) => movie?.movieId === cardId)) {
-          // Add the movie to the user's list
-          setUser((prevUser) => ({
-            ...prevUser,
-            movies: [
-              ...prevUser.movies,
-              {
-                title: movieTitle,
-                movieId: cardId,
-                imageUrl: imageUrl,
-                isFavorite: true,
-                isWatchlist: false,
-              },
-            ],
-          }));
-          setOpenMenu(!openMenu);
-        }
-      } else {
-        // Movie already exists in user's list
-        console.log("Movie already exists in user's list");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  const watchlistHandler = async (movieTitle, cardId, userId, imageUrl) => {
-    try {
-      const movieRes = await axios.post(
-        " http://localhost:3000/favorite-movie",
-        {
-          title: movieTitle,
-          movieId: cardId.toString(),
-          imageUrl: imageUrl,
-          userId: userId,
-          isFavorite: false,
-          isWatchlist: true,
-        },
-        { withCredentials: true }
-      );
-      if (movieRes.status === 201) {
-        // Check if the movie is already in the user's list
-        if (!user.movies.some((movie) => movie?.movieId === cardId)) {
-          // Add the movie to the user's list
-          setUser((prevUser) => ({
-            ...prevUser,
-            movies: [
-              ...prevUser.movies,
-              {
-                title: movieTitle,
-                movieId: cardId,
-                imageUrl: imageUrl,
-                isFavorite: true,
-                isWatchlist: false,
-              },
-            ],
-          }));
-          setOpenMenu(!openMenu);
-        }
-      } else {
-        // Movie already exists in user's list
-        console.log("Movie already exists in user's list");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-    
+    };
+
     return (
         <div className="carousel">
             {
@@ -211,57 +166,80 @@ const ElaCard = ({ data, mediaType }) => {
                                                     />
                                                 </svg>
                                             </IconButton>
+                                        </MenuHandler>
+
+                                        <MenuList>
+                                            <MenuItem
+                                                onClick={() =>
+                                                    favoriteHanlder(
+                                                        item.title,
+                                                        item.id,
+                                                        user?._id,
+                                                        item.poster_path
+                                                    )
+                                                }
+                                                className="flex items-center gap-2"
+                                            >
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 24 24"
+                                                    fill="currentColor"
+                                                    color="red"
+                                                    className="w-6 h-6 "
+                                                >
+                                                    <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                                                </svg>
 
 
-                        <Typography variant="small" className="font-medium">
-                          Favorite
-                        </Typography>
-                      </MenuItem>
-                      <hr className="my-2" />
-                      <MenuItem
-                        onClick={() =>
-                          watchlistHandler(
-                            item.title || item.name,
-                            item.id,
-                            user?._id,
-                            item.poster_path
-                          )
-                        }
-                        className="flex items-center gap-2"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          fill="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
+                                                <Typography variant="small" className="font-medium">
+                                                    Favorite
+                                                </Typography>
+                                            </MenuItem>
+                                            <hr className="my-2" />
+                                            <MenuItem
+                                                onClick={() =>
+                                                    watchlistHandler(
+                                                        item.title,
+                                                        item.id,
+                                                        user?._id,
+                                                        item.poster_path
+                                                    )
+                                                }
+                                                className="flex items-center gap-2"
+                                            >
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 24 24"
+                                                    fill="currentColor"
+                                                    className="w-6 h-6"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z"
+                                                        clipRule="evenodd"
+                                                    />
+                                                </svg>
 
-                        <Typography variant="small" className="font-medium">
-                          Watchlist
-                        </Typography>
-                      </MenuItem>
-                    </MenuList>
-                  </Menu>
+                                                <Typography variant="small" className="font-medium">
+                                                    Watchlist
+                                                </Typography>
+                                            </MenuItem>
+                                        </MenuList>
+                                    </Menu>
+                                </div>
+                                <div className="textBlock">
+                                    <span className="title">{item.title || item.name}</span>
+                                    <span className="date">
+                                        {dayjs(item.release_Date).format("MMM D, YYYY")}
+                                    </span>
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
-                <div className="textBlock">
-                  <span className="title">{item.title || item.name}</span>
-                  <span className="date">
-                    {dayjs(item.release_Date).format("MMM D, YYYY")}
-                  </span>
-                </div>
-              </div>
-            );
-          })}
+            }
         </div>
-      }
-    </div>
-  );
+    );
 };
 
 export default ElaCard;
