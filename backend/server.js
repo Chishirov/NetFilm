b49a2cf5037e3363de882b84bc826446ec830839
omar <yossef>
2024-03-17 13:40:49 +0100

    render propieren

--- backend/server.js
+++ backend/server.js
@@ -14,27 +14,30 @@ const PORT = process.env.PORT || 3000;
 app.use(helmet());
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs
 });
 app.use(limiter);
 
 // Middleware
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
     credentials: true,
-    origin: "https://movie-website-w8eb.onrender.com",
+    origin: "https://movie-website-mqnf.onrender.com",
   })
 );
-app.set("trust proxy", 1)
+app.set("trust proxy", 1);
 app.use(cookieParser());
 
 // Database connection with error handling
-mongoose.connect(process.env.DB_URL).then(() => {
-  console.log('Connected to the database successfully');
-}).catch((error) => {
-  console.error('Database connection failed', error);
-});
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Connected to the database successfully");
+  })
+  .catch((error) => {
+    console.error("Database connection failed", error);
+  });
 
 // Routes
 app.get("/test", (req, res) => {
@@ -46,6 +49,5 @@ app.use("/", router);
 app.listen(PORT, () => {
   console.log(`
   Server is listening on port http://localhost:${PORT} 
-  test>> http://localhost:${PORT}/test`
-  );
+  test>> http://localhost:${PORT}/test`);
 });
