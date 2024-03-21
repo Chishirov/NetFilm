2cd2d25296148ce91dc3dee0d81ec23825c095c7
omar <yossef>
2024-03-21 10:13:27 +0100

    update cors in backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routers/router.jsx";
 import axios from "axios";
 import url from "../config/config.js";
+
 axios.defaults.baseURL = url;
 axios.defaults.withCredentials = true;
 
 function App() {
+  console.log("url in frontend ap.jsx", url);
   return (
     <div>
       <RouterProvider router={router}></RouterProvider>
